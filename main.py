4e602338f28fef3bc2c1f80a648676bc2a55d184
RanitManik <138437760+RanitManik@users.noreply.github.com>
2023-12-23 12:42:18 +0000

    added file support

--- main.py
+++ main.py
@@ -6,6 +6,21 @@ import openai
 token = os.environ['SECRET_KEY']
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# file = input("Enter 1, 2, or 3 for loading the chat:\n ")
+file = "2"
+match(file):
+  case "1":
+    file = "chat1.txt"
+  case "2":
+    file = "chat2.txt"
+  case "3": 
+    file = "chat3.txt"
+  case _:
+    print("Invalid choice.")
+    exit()
+
+with open(file, "r") as f:
+  chat = f.read()
 
 class MyClient(discord.Client):
 
@@ -13,25 +28,31 @@ class MyClient(discord.Client):
     print(f'Logged on as {self.user}!')
 
   async def on_message(self, message):
-    print(f'Message from {message.author}: {message.content}')
-    if self.user != message.author and self.user in message.mentions:
-      response = openai.chat.completions.create(
-          model="gpt-3.5-turbo",
-          messages=[{
-              "role": "system",
-              "content": "You are a helpful assistant."
-          }, {
-              "role": "user",
-              "content": message.content
-          }],
-          temperature=1,
-          max_tokens=256,
-          top_p=1,
-          frequency_penalty=0,
-          presence_penalty=0)
-      channel = message.channel
-      messageToSend = response.choices[0].message.content
-      await channel.send(messageToSend)
+    global chat
+    try:
+      chat += f"{message.author}: {message.content}\n"
+      print(f'Message from {message.author}: {message.content}')
+      if self.user != message.author:
+        response = openai.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{
+                "role": "system",
+                "content": f"{chat}\nHarryGPT:"
+            }, {
+                "role": "user",
+                "content": message.content
+            }],
+            temperature=1,
+            max_tokens=500,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0)
+        channel = message.channel
+        messageToSend = response.choices[0].message.content
+        await channel.send(messageToSend)
+    except Exception as e:
+      print(e)
+      chat = ""
 
 
 intents = discord.Intents.default()
